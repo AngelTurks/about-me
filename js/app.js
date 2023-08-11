ed04dcf0dea47b72ed4bd26ac146526c08b7b98b
ANGEL TURKS <angelturks17@gmail.com>
2023-08-11 02:48:41 -0400

    added more js

--- js/app.js
+++ js/app.js
@@ -13,3 +13,35 @@ if (sports === 'y' || sports === 'yes') {
 } else {
   alert("Wrong, Angel does like sports!");
 }
+
+let music = prompt("Do Angel Like music?");
+
+if (music === 'y' || music === 'yes') {
+  alert("Correct, Angel does like music!");
+} else {
+  alert("Wrong, Angel does like music!");
+}
+
+let gaming = prompt("Do Angel like gaming?");
+
+if (gaming === 'y' || gaming === 'yes') {
+  alert("Correct, Angel does like gaming!");
+} else {
+  alert("Wrong, Angel does like gaming!");
+}
+
+let reading = prompt("Does Angel like the reading?");
+
+if (reading === 'y' || reading === 'yes') {
+  alert("Correct, Angel does like reading!");
+} else {
+  alert("Wrong, Angel does like reading!");
+}
+
+let braiding = prompt("Do Angel like braiding hair?");
+
+if (braiding === 'y' || braiding === 'yes') {
+  alert("Correct, Angel does like braiding hair!");
+} else {
+  alert("Wrong, Angel does like braiding hair!");
+}
